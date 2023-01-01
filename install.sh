04e99333718af62ed8762660b7a54c7ec03c97a7
casjay <git-admin@casjaysdev.com>
2023-01-01 17:24:59 -0500

    🗃️ Committing everything that changed 🗃️

--- install.sh
+++ install.sh
@@ -20,17 +20,27 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 [ -n "$(builtin type -P git)" ] || { echo "This script requires git" && exit 1; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo "Setting up rpm development scripts"
 git clone -q "https://github.com/rpm-devel/tools" "/tmp/rpm-dev-tools" || { echo "Failed to clone the repo" && exit 1; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if [ -d "$HOME/.local/bin" ]; then
-  cp -R "/tmp/rpm-dev-tools/." "$HOME/.local/bin"
+  U_BIN="$HOME/.local/bin"
+  cp -R "/tmp/rpm-dev-tools/bin/." "$HOME/.local/bin"
 elif [ -d "$HOME/bin" ]; then
-  cp -R "/tmp/rpm-dev-tools/." "$HOME/bin"
+  U_BIN="$HOME/bin"
+  cp -R "/tmp/rpm-dev-tools/bin/." "$HOME/bin"
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 [ -f "/tmp/rpm-dev-tools" ] && cp -Rf "/tmp/rpm-dev-tools/.rpmmacros" "$HOME/.rpmmacros"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-[ ! -d "/tmp/rpm-dev-tools" ] || rm -Rf "/tmp/rpm-dev-tools"
+if [ -f "$HOME/.rpmmacros" ] && [ -f "$U_BIN/create-container.sh" ]; then
+  echo "Setup has completed successfully"
+  rm -Rf "/tmp/rpm-dev-tools"
+  exit 0
+else
+  echo "Setup has failed"
+  exit 1
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # End application
 # ex: ts=2 sw=2 et filetype=sh
