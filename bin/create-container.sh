5b64e21a36867cdc8e0a933d88981541de7f4afd
casjay <git-admin@casjaysdev.com>
2023-01-01 19:41:06 -0500

    🗃️ Committing everything that changed 🗃️

--- bin/create-container.sh
+++ bin/create-container.sh
@@ -112,8 +112,8 @@ if [ ! -d "/usr/local/share/CasjaysDev/scripts" ]; then
   __docker_execute -q /usr/local/share/CasjaysDev/scripts/install.sh
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-__docker_execute -q bash -c "$(curl -q -LSsf "https://github.com/rpm-devel/tools/raw/main/install.sh")"
+__docker_execute -q git clone "https://github.com/rpm-devel/tools" "/tmp/rpm-devel"
+__docker_execute -q bash -c "/tmp/rpm-devel/install.sh"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #__docker_execute bash -c "pkmgr script https://github.com/pkmgr/centos/raw/main/scripts/development.sh"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
