5de3dac6b975ff38aa3824073c57332c84eea56d
casjay <git-admin@casjaysdev.com>
2023-01-01 23:08:29 -0500

    🗃️ Committing everything that changed 🗃️

--- bin/rpmbuild.sh
+++ bin/rpmbuild.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+clear
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 VERNAME="el"
 DISTRO="RHEL"
 ARCH="$(uname -m)"
