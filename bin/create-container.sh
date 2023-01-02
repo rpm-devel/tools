e841a7169b7e534993cfa40f12c7abe3a34ef162
casjay <git-admin@casjaysdev.com>
2023-01-01 19:35:32 -0500

    🗃️ Committing everything that changed 🗃️

--- bin/create-container.sh
+++ bin/create-container.sh
@@ -42,10 +42,10 @@ __docker_execute() {
   local ARGS="$*"
   echo "Executing: $ARGS" && sleep 1
   if [ "$SILENT" = "true" ]; then
-    docker exec -it $C_NAME "$ARGS" &>/dev/null
+    docker exec -it $C_NAME $ARGS &>/dev/null
     exitCode=$?
   else
-    docker exec -it $C_NAME "$ARGS"
+    docker exec -it $C_NAME $ARGS
     exitCode=$?
   fi
   if [ $exitCode -eq 0 ]; then
